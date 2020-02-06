92189ee6711b4d0c61e60d9b5ab74bc566b23e0d
naveensugumar <36404382+naveensugumar@users.noreply.github.com>
2020-02-06 19:44:39 +0530

    commit1
    Initial version of number to word converter project validation with boundary value testcases

---
+++ WordConverter/src/main/java/com/bank/application/service/impl/NumberConverterServiceImpl.java
@@ -0,0 +1,36 @@
+package com.bank.application.service.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.bank.application.service.NumberConverterService;
+import com.bank.application.util.ServiceConstants;
+
+/**
+ * @author NaveenSugumar
+ *
+ */
+
+@Repository
+public class NumberConverterServiceImpl implements NumberConverterService {
+
+	@Override
+	public String convertNumToWord(String value) throws NumberFormatException  {
+		StringBuilder wordConvertorString = new StringBuilder();
+		try {
+		int number = Integer.parseInt(value);
+		checkMinMaxNumber(number);
+		} catch (Exception e) {
+			throw new NumberFormatException(ServiceConstants.INVALID_NUMBER);
+		}
+
+		return wordConvertorString.toString();
+	}
+
+	private void checkMinMaxNumber(int number) {
+		if (number <= 0) {
+			throw new NumberFormatException(ServiceConstants.INVALID_NUMBER);
+		} else if(number > 999999) {
+			throw new NumberFormatException(ServiceConstants.INVALID_NUMBER);
+		}
+	}
+}
---
+++ WordConverter/src/main/java/com/bank/application/util/ServiceConstants.java
@@ -0,0 +1,15 @@
+package com.bank.application.util;
+
+public interface ServiceConstants {
+
+	
+	String LAKH = "LAKH";
+	String THOUSAND = "THOUSAND";
+	String HUNDRED = "HUNDRED";
+	String CRORE = "CRORE";
+	String EMPTY = " ";
+	String AND = "AND";
+	String DOT = ".";
+	String INVALID_NUMBER = "Enter number between 1 and 999999. Please re-check.";
+	
+}
